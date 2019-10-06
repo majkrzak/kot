bb183a7b9b3d75f41ec2a051ed805b03cb1e6923
Piotr Majkrzak <piotr@majkrzak.dev>
2019-10-06 13:13:11 +0300

    Add setup script with dependencies

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name='kot',
+    author='Piotr Majrzak',
+    author_email='piotr@majkrzak.dev',
+    license='Permission to use, copy, modify, and distribute this software for any '
+            + 'purpose with or without fee is hereby granted, provided that the above '
+            + 'copyright notice and this permission notice appear in all copies.',
+    packages=[
+        'src',
+    ],
+    package_dir={'src': './src'},
+    install_requires=[
+        'requests',
+        'lxml'
+    ],
+    setup_requires=[
+        'wheel'
+    ]
+)
