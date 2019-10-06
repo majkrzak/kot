4f3f810ae4fea2698b6d9423da4fe27646046b62
Timothy Baney <timbaney1989@gmail.com>
2019-10-06 11:14:07 -0500

    Begin named argument parsing by adding argparser stdlib to init

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ setuptools.setup(
     name='kot',
     author='Piotr Majrzak',
     author_email='piotr@majkrzak.dev',
-    license='Permission to use, copy, modify, and distribute this software for any '
-            + 'purpose with or without fee is hereby granted, provided that the above '
-            + 'copyright notice and this permission notice appear in all copies.',
+    license='''
+        Permission to use, copy, modify, and distribute this software for any
+        purpose with or without fee is hereby granted, provided that the above
+        copyright notice and this permission notice appear in all copies.''',
     packages=[
         'kot',
     ],
---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+import argparse
+
+parser=argparse.ArgumentParser()
+
+parser.add_argument('--structure', help='JSON file to resolve')
+args = parser.parse_args()
